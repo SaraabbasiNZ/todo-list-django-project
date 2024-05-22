8c3598b7d7a718a491424f2ac3cdd963b5369e7b
SaraabbasiNZ Abbasi <Sara.abbasi54@yahoo.com>
2024-05-22 23:00:08 +0000

    Add fun to create new task

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_view, todo_list_view
+from .views import home_view, todo_list_view, todo_item_create
 
 
 app_name = 'todo_app'
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('list/', todo_list_view, name='todo_list')
+    path('list/', todo_list_view, name='todo_list'),
+    path('create/', todo_item_create, name='todo_item_create'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import TodoItem
 from django.contrib.auth.decorators import login_required
+from .forms import TodoItemForm
 
 # Create your views here.
 def home_view(request):
@@ -23,3 +24,16 @@ def todo_list_view(request):
         
 
     return render(request, 'todo_app/todo_list.html', {'todolist':query})
+
+@login_required(login_url='/accounts/login')
+def todo_item_create(request):
+    user = request.user
+    if request.method == 'POST':
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit= False)
+            instance.owner = user
+            instance.save()
+            return redirect('todo_app:todo_list')
+    form = TodoItemForm()
+    return render(request, 'todo_app/create_todo_item.html', {'form':form})
