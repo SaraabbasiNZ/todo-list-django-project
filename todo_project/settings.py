b37703c016841c709fc03a43a017eb4c95304bc6
SaraabbasiNZ Abbasi <Sara.abbasi54@yahoo.com>
2024-05-26 19:04:11 +0000

    Create first stylesheet

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -138,4 +138,15 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR,'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
