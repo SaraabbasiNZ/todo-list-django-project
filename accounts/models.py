e43ebba5c5eda385092087b1a3bc697f1acc52c6
SaraabbasiNZ Abbasi <Sara.abbasi54@yahoo.com>
2024-05-26 23:53:46 +0000

    complete delete function and add style

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# class ContactUs(models.Model):
+#     title = models.CharField(max_length=50)
+#     message = models.TextField()
+#     email = models.EmailField(max_length=254)
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TodoItem
 from django.contrib.auth.decorators import login_required
 from .forms import TodoItemForm
