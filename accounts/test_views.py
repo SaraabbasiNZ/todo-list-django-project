8ab8d330b787cdf315fbbe5260b2cfdbb0a577c8
SaraabbasiNZ Abbasi <Sara.abbasi54@yahoo.com>
2024-06-13 01:18:13 +0000

    Testing account views

---
+++ accounts/test_views.py
@@ -0,0 +1,56 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
+
+class AccountViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.signup_url = reverse('accounts:signup')
+        self.login_url = reverse('accounts:login')
+        self.logout_url = reverse('accounts:logout')
+        self.user_credentials = {
+            'username': 'testuser',
+            'password': 'testpassword123',
+            'email': 'testuser@example.com'  
+        }
+        self.user = User.objects.create_user(**self.user_credentials)
+
+    def test_signup_view_get(self):
+        response = self.client.get(self.signup_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+        self.assertIsInstance(response.context['form'], CustomUserCreationForm)
+
+    def test_signup_view_post(self):
+        response = self.client.post(self.signup_url, {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password1': 'newpassword123',
+            'password2': 'newpassword123'
+        })
+        if response.status_code == 200:
+            print(response.context['form'].errors)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    def test_login_view_get(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+        self.assertIsInstance(response.context['form'], AuthenticationForm)
+
+    def test_login_view_post(self):
+        response = self.client.post(self.login_url, self.user_credentials)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_logout_view_post(self):
+        self.client.login(**self.user_credentials)
+        response = self.client.post(self.logout_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
