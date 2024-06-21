7b878bb1e69037bfe94b5762caf433456ae08ab8
SaraabbasiNZ Abbasi <Sara.abbasi54@yahoo.com>
2024-06-21 04:30:06 +0000

    adhering view.py codes to 79-80 characters per line

--- todo_app/views.py
+++ todo_app/views.py
@@ -8,27 +8,33 @@ from django.contrib import messages
 
 
 def home_view(request):
-    todo_list = TodoItem.objects.filter(is_private = False).order_by('priority', 'date')
+    todo_list = TodoItem.objects.filter(
+        is_private=False
+    ).order_by('priority', 'date')
     return render(request, 'todo_app/home.html', {'todolist': todo_list})
 
 
 @login_required(login_url='/accounts/login')
 def todo_list_view(request):
     user = request.user
-    query = TodoItem.objects.filter(owner = user).order_by('priority', 'date')
+    query = TodoItem.objects.filter(owner=user).order_by('priority', 'date')
 
     if request.method == 'POST':
         checked_list = request.POST.getlist('checked')
         checked_list = [int(i) for i in checked_list]
         for todo_item in query:
             if todo_item.id in checked_list:
-                TodoItem.objects.filter(id = todo_item.id).update(checked=True)
+                TodoItem.objects.filter(id=todo_item.id).update(checked=True)
             else:
-                TodoItem.objects.filter(id = todo_item.id).update(checked=False)
+                TodoItem.objects.filter(id=todo_item.id).update(checked=False)
         return redirect(reverse('todo_app:todo_list'))
-    
+
     todo_list_len = len(query)
-    return render(request, 'todo_app/todo_list.html', {'todolist': query,'todo_list_len': todo_list_len})
+    return render(
+        request, 'todo_app/todo_list.html', {
+            'todolist': query, 'todo_list_len': todo_list_len
+        }
+    )
 
 
 @login_required(login_url='/accounts/login')
@@ -44,15 +50,25 @@ def todo_item_create(request):
                 instance = form.save(commit=False)
                 instance.owner = user
                 instance.save()
-                messages.success(request,'Task successfully created')
+                messages.success(request, 'Task successfully created')
                 return redirect('todo_app:todo_list')
             except ValidationError:
-                messages.error(request,'Task Date can not be in the past')
+                messages.error(request, 'Task Date can not be in the past')
                 form = TodoItemForm(request.POST)
-                return render(request, 'todo_app/create_todo_item.html', {'form':form, 'categories': categories, 'priorities':priorities})
+                return render(
+                    request, 'todo_app/create_todo_item.html', {
+                        'form': form, 'categories': categories,
+                        'priorities': priorities
+                    }
+                )
     else:
-        form = TodoItemForm(categories=categories, priorities=priorities)    
-    return render(request, 'todo_app/create_todo_item.html', {'form':form, 'categories': categories, 'priorities':priorities})
+        form = TodoItemForm(categories=categories, priorities=priorities)
+    return render(
+        request, 'todo_app/create_todo_item.html', {
+            'form': form, 'categories': categories,
+            'priorities': priorities
+        }
+    )
 
 
 @login_required(login_url='/accounts/login')
@@ -66,15 +82,26 @@ def edit_todo_item(request, id):
         if form.is_valid():
             try:
                 form.save()
-                messages.success(request,'Task successfully edited')
+                messages.success(request, 'Task successfully edited')
                 return redirect('todo_app:todo_list')
             except ValidationError:
-                messages.error(request,'Task Date can not be in the past')
+                messages.error(request, 'Task Date can not be in the past')
                 form = TodoItemForm(request.POST, instance=todo_item)
-                return render(request, 'todo_app/edit_todo_item.html', {'form': form, 'categories': categories, 'priorities':priorities})
+                return render(
+                    request, 'todo_app/edit_todo_item.html', {
+                        'form': form, 'categories': categories,
+                        'priorities': priorities
+                    }
+                )
     else:
-        form = TodoItemForm(instance=todo_item, categories=categories, priorities=priorities)    
-    return render(request, 'todo_app/edit_todo_item.html', {'form': form, 'categories': categories, 'priorities':priorities})
+        form = TodoItemForm(
+            instance=todo_item, categories=categories,
+            priorities=priorities)
+    return render(
+        request, 'todo_app/edit_todo_item.html', {
+            'form': form, 'categories': categories, 'priorities': priorities
+        }
+    )
 
 
 def delete_todo_item(request, id):
@@ -82,7 +109,7 @@ def delete_todo_item(request, id):
     if request.method == 'POST':
         if item.owner == request.user:
             item.delete()
-            messages.success(request,'Task successfully deleted')
+            messages.success(request, 'Task successfully deleted')
             return redirect('todo_app:todo_list')
         else:
             return redirect('todo_app:todo_list')
